4e3d10fb3ca30bc6d82482ac59440ea761100365
agolautner <lautner.agoston@gmail.com>
2022-02-10 14:04:40 +0100

    add flash msg feature

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 //write the code here
+window.onload = () => {
+    const flashes = document.querySelectorAll('.flash');
+    setInterval(() => {
+        for (let flash of flashes) {
+            flash.classList.toggle('red');
+        }
+    }, 1000);
+}
